e899cfaff78e522603ef2aeb905efa633de4ed55
Carlos Becerra Hernández <carlosbecerra.dev@gmail.com>
2021-12-05 13:50:39 -0500

    :pencil: Corrigiendo el nombre del archivo.

--- src/views/jefe/ConsultarReporteDeTipoDeIncidenciasView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="consultar-reporte-de-tipo-de-incidencia-view"></div>
-</template>
-
-<script>
-export default {
-  name: "ConsultarReporteDeTipoDeIncidenciasView",
-};
-</script>
---
+++ src/views/jefe/ConsultarReporteDeTiposDeIncidenciaView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="consultar-reporte-de-tipos-de-incidencia-view"></div>
+</template>
+
+<script>
+export default {
+  name: "ConsultarReporteDeTiposDeIncidenciaView",
+};
+</script>
