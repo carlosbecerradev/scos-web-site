f1ef8cc5ee059a89b8104dd31084898266ee87f3
Carlos Becerra Hernández <carlosbecerra.dev@gmail.com>
2021-12-06 22:20:15 -0500

    :hammer: Agregando las vistas para el rol coordinador.

--- src/views/coordinador/ConsultarOrdenesDeServicioView.vue
+++ src/views/coordinador/ConsultarOrdenesDeServicioView.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="consultar-ordenes-de-servicio-view"></div>
+  <div class="consultar-ordenes-de-servicio-view">
+    <coordinador-layout></coordinador-layout>
+  </div>
 </template>
 
 <script>
+import CoordinadorLayout from "@/layout/CoordinadorLayout.vue";
+
 export default {
   name: "ConsultarOrdenesDeServicioView",
+  components: {
+    CoordinadorLayout,
+  },
 };
 </script>
--- src/views/coordinador/GenerarOrdenDeServicioView.vue
+++ src/views/coordinador/GenerarOrdenDeServicioView.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="generar-orden-de-servicio-view"></div>
+  <div class="generar-orden-de-servicio-view">
+    <coordinador-layout></coordinador-layout>
+  </div>
 </template>
 
 <script>
+import CoordinadorLayout from "@/layout/CoordinadorLayout.vue";
+
 export default {
   name: "GenerarOrdenDeServicioView",
+  components: {
+    CoordinadorLayout,
+  },
 };
 </script>
