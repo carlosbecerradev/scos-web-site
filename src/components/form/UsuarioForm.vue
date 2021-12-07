aa2b0172989f0937aa7c10a9cdf4804dcc38ec84
Carlos Becerra Hernández <carlosbecerra.dev@gmail.com>
2021-12-06 22:46:20 -0500

    :construction: WIP: formulario de usuario en progreso.

--- src/components/form/UsuarioForm.vue
+++ src/components/form/UsuarioForm.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="usuario-form"></div>
+  <div class="usuario-form">
+    <p class="lead">Formulario de Usuario</p>
+    <b-form>
+      <b-form-group label="Email address:">
+        <b-form-input
+          id="input-1"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group label="Activo">
+        <b-form-select class="mb-3">
+          <b-form-select-option value="null"
+            >Please select an option</b-form-select-option
+          >
+          <b-form-select-option value="1">Si</b-form-select-option>
+          <b-form-select-option value="0" disabled>No</b-form-select-option>
+        </b-form-select>
+      </b-form-group>
+      <b-button variant="primary">Button</b-button>
+    </b-form>
+  </div>
 </template>
 
 <script>
--- src/views/administrador/GestionarUsuarioView.vue
+++ src/views/administrador/GestionarUsuarioView.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="gestionar-usuario-view">
-    <administrador-layout></administrador-layout>
+    <administrador-layout>
+      <usuario-form></usuario-form>
+    </administrador-layout>
   </div>
 </template>
 
 <script>
 import AdministradorLayout from "@/layout/AdministradorLayout.vue";
+import UsuarioForm from "@/components/form/UsuarioForm.vue";
 
 export default {
   name: "GestionarUsuarioView",
   components: {
     AdministradorLayout,
+    UsuarioForm,
   },
 };
 </script>
